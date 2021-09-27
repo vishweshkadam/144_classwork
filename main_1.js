9d25459fed679823ef4a26ee9d25a7beb9ac05dd
vishweshkadam <75071313+vishweshkadam@users.noreply.github.com>
2021-09-27 18:48:04 +0530

    Add files via upload

---
+++ main_1.js
@@ -0,0 +1,34 @@
+noseX=0
+noseY=0
+ function preload(){
+Clown=loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png")
+}
+function setup(){
+    
+   canvas=createCanvas(300,300)
+   canvas.center()
+   video=createCapture(VIDEO);
+  video.size(300,300)
+  video.hide()
+
+   posenet=ml5.poseNet(video,modelLoad)
+posenet.on("pose",gotpose)
+
+}
+
+
+function modelLoad(){
+    console.log("my name is vishwesh")
+
+}
+function gotpose(result){
+    //console.log(result);
+    noseX=result[0].pose.nose.x;
+    noseY=result[0].pose.nose.y;
+    console.log(noseX)
+    console.log(noseY)
+}
+ function draw(){
+     image(video,0,0,300,300)
+     image(Clown,noseX-12,noseY-15,20,20)
+ }
